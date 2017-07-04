4b44ca8c5e875c884900455dab9ce6b9ceb6324e
daniel.karandikar <DanielKarandikar@minsterlogistics.com>
2017-07-04 16:55:06 +0100

    Up to tutorial 8; added Checkboxes for creating and editing instructors

---
+++ ContosoUniversity2/ViewModels/AssignedCourseData.cs
@@ -0,0 +1,9 @@
+﻿namespace ContosoUniversity2.ViewModels
+{
+    public class AssignedCourseData
+    {
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public bool Assigned { get; set; }
+    }
+}
