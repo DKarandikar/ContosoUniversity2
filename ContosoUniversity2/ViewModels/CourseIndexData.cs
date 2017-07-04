ed8f70e64fd50c9a6d9ae236ccb3df4de30ea8bf
daniel.karandikar <DanielKarandikar@minsterlogistics.com>
2017-07-04 16:01:31 +0100

    Added select option on Courses Page, this displays all students that are enrolled on the course. Added ViewModel  to this effect

---
+++ ContosoUniversity2/ViewModels/CourseIndexData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using ContosoUniversity2.Models;
+
+namespace ContosoUniversity2.ViewModels
+{
+    public class CourseIndexData
+    {
+        public IEnumerable<Course> Courses { get; set; }
+        public IEnumerable<Enrollment> Enrollments { get; set; }
+    }
+}
