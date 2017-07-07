ba44dae42fe02af6f880ab6793e0d7c5876664cf
daniel.karandikar <DanielKarandikar@minsterlogistics.com>
2017-07-07 15:05:31 +0100

    Implemented alphabetic sort and modulo operation on a page called Extra

--- ContosoUniversity2/Controllers/HomeController.cs
+++ ContosoUniversity2/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Data.Entity;
+using System.Globalization;
 
 using ContosoUniversity2.DAL;
 using ContosoUniversity2.ViewModels;
@@ -42,6 +43,116 @@ namespace ContosoUniversity2.Controllers
             var result = db.Database.SqlQuery<CourseStatisticsData>("StudentNoByCourseID").ToList();
 
             return View(result);
+        }
+
+        //[HttpPost]
+        public ActionResult Extra(ExtraData model)
+        {
+            ViewBag.WordError = false; //Tells whether an error has been made in inputting words in the alphabetic comparison
+            // If an error has been made then the HTML changes to reflect this
+
+            int? numberA = model.A;
+            int? numberB = model.B;
+            int remainder;
+
+            if (model.B == 0)
+            {
+                ModelState.AddModelError("Divide0", "Division by 0 is not allowed");
+                return View(model);
+            }
+            if (model.B < 0 || model.A < 0 )
+            {
+                ModelState.AddModelError("Less0", "Negative numbers aren't permitted");
+                return View(model);
+            }
+
+            if (numberA == null) {remainder = 0;}
+            else{ remainder = (int)numberA;}
+
+            // Provided there are no null or 0; simply subtract B from A until the result is >= B
+            // Could move this to a function; but it's so small
+            if (numberA != null && numberB != null)
+            {                
+                while (remainder >= numberB)
+                {
+                    remainder = remainder - (int) numberB;
+                }
+            }
+            model.Result = remainder;
+
+            model.ResultWord = CompareWords(model.FirstWord, model.SecondWord);
+            
+            return View(model);
+        }
+
+        private string CompareWords(string A, string B)
+        {
+            Boolean found = false;
+            String result = "These are the same";
+
+            if (A == null || B == null)
+            {
+                ViewBag.WordError = true;
+                return "";
+            }
+
+            if (!((A.All(Char.IsLetter)) && (B.All(Char.IsLetter)))){
+                ViewBag.WordError = true;
+                return "Only letters allowed";
+                
+            }
+
+            List<string> letters = new List<string>{ "a","b","c","d","e","f","g","h","i","j","k","l","m"
+                ,"n","o","p","q","r","s","t","u","v","w","x","y","z" };
+
+            Dictionary<string, int> values = new Dictionary<string, int>();
+            int i = 1;
+            foreach (string s in letters)
+            {
+                values[s] = i;
+                i += 1;
+            }
+
+            int sRef = 0; //This will tick along the string
+
+            while ((!found) && (sRef < A.Length) && (sRef < B.Length))
+            {
+                string nth_A = Char.ToLower(A[sRef]).ToString();
+                string nth_B = Char.ToLower(B[sRef]).ToString();
+
+                if (values[nth_A] < values[nth_B])
+                {
+                    found = true;
+                    result = A;
+                }
+                else if (values[nth_A] > values[nth_B])
+                {
+                    found = true;
+                    result = B;
+                } else
+                {
+                    sRef += 1;
+                }
+            }
+
+            if (result == "These are the same")
+            {
+                // If they have the same word for the length of one; but one is longer; then the shorter one goes first
+                if (A.Length > B.Length)
+                {
+                    return B;
+                }
+                else if (B.Length > A.Length)
+                {
+                    return A;
+                }
+                else
+                {
+                    ViewBag.WordError = true;
+                }
+               
+            }
+            return result;
         }
 
         protected override void Dispose(bool disposing)
---
+++ ContosoUniversity2/ViewModels/ExtraData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using ContosoUniversity2.Models;
+
+namespace ContosoUniversity2.ViewModels
+{
+    public class ExtraData
+    {
+        public int? A { get; set; }
+        public int? B { get; set; }
+        public int? Result { get; set; }
+
+        public string FirstWord { get; set; }
+        public string SecondWord { get; set; }
+        public string ResultWord { get; set; }
+    }
+}
